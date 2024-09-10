5cc39d638cf18fd253389ef8e70ae54a9a0ce588
frudisch <florian.rudisch@gmail.com>
2024-09-10 20:05:02 +0200

    * Use simple math.random to create random number instead of library

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const core = require('@actions/core');
-const random = require('random');
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-    const number = random.int((min = 0), (max = 1000))
+    const number = Math.floor(Math.random()*1000);
     core.setOutput("random-number", number);
 } catch (error) {
     core.setFailed(error.message);
